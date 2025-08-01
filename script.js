1ffdcc701b40d209abb1ef096ca2c3be913a8465
NeymarkingingYT <NeymarkingingYT@gmail.com>
2025-08-01 15:04:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,30 @@ function loadWorkspace() {
     Blockly.Xml.domToWorkspace(xml, workspace);
   }
 }
+
+Blockly.Blocks['move_sprite'] = {
+  init: function () {
+    this.appendDummyInput()
+      .appendField("move sprite by")
+      .appendField(new Blockly.FieldNumber(10), "X")
+      .appendField("x and")
+      .appendField(new Blockly.FieldNumber(0), "Y")
+      .appendField("y");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+  }
+};
+
+Blockly.JavaScript['move_sprite'] = function(block) {
+  const dx = block.getFieldValue('X');
+  const dy = block.getFieldValue('Y');
+  return `
+    const sprite = document.getElementById("sprite");
+    const left = parseInt(sprite.style.left || 0);
+    const top = parseInt(sprite.style.top || 0);
+    sprite.style.left = (left + ${dx}) + "px";
+    sprite.style.top = (top + ${dy}) + "px";
+  `;
+};
+
